6d98dc06ff921e73dc004d6c077dfad16304729d
Eric Rodrigues Pires <eric@eric.dev.br>
2026-09-23 10:23:30 -0300

    ping-viewer-next: fix x86_64 cross-compilation, refactor

--- nix/ping-viewer-next/default.nix
+++ nix/ping-viewer-next/default.nix
@@ -15,38 +15,26 @@ let
     inherit dockerTag;
   };
 
-  backend-x86_64 = pkgs.pkgsCross.x86_64-multiplatform.callPackage ./backend.nix {
-    ping-viewer-next-frontend = frontend;
-  };
-  docker-x86_64 = pkgs.pkgsCross.x86_64-multiplatform.callPackage ./docker.nix {
-    ping-viewer-next = backend-aarch64;
-    inherit dockerTag;
-  };
-  backend-aarch64 = pkgs.pkgsCross.aarch64-multiplatform.callPackage ./backend.nix {
-    ping-viewer-next-frontend = frontend;
-  };
-  docker-aarch64 = pkgs.pkgsCross.aarch64-multiplatform.callPackage ./docker.nix {
-    ping-viewer-next = backend-aarch64;
-    inherit dockerTag;
-  };
-  backend-armv7l = pkgs.pkgsCross.armv7l-hf-multiplatform.callPackage ./backend.nix {
-    ping-viewer-next-frontend = frontend;
-  };
-  docker-armv7l = pkgs.pkgsCross.armv7l-hf-multiplatform.callPackage ./docker.nix {
-    ping-viewer-next = backend-armv7l;
-    inherit dockerTag;
-  };
+  mkCross =
+    suffix: pkgsCross:
+    let
+      backendCross = pkgsCross.callPackage ./backend.nix { ping-viewer-next-frontend = frontend; };
+    in
+    {
+      "backend-${suffix}" = backendCross;
+      "docker-${suffix}" = pkgsCross.callPackage ./docker.nix {
+        ping-viewer-next = backendCross;
+        inherit dockerTag;
+      };
+    };
 in
 {
   inherit
     frontend
     backend
     docker
-    backend-x86_64
-    docker-x86_64
-    backend-aarch64
-    docker-aarch64
-    backend-armv7l
-    docker-armv7l
     ;
 }
+// (mkCross "x86_64" pkgs.pkgsCross.gnu64)
+// (mkCross "aarch64" pkgs.pkgsCross.aarch64-multiplatform)
+// (mkCross "armv7l" pkgs.pkgsCross.armv7l-hf-multiplatform)
