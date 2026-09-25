d5f4b477a62d69a16536a89e8120927f4ba5c33b
Eric Rodrigues Pires <eric@eric.dev.br>
2026-09-25 10:16:17 -0300

    ping-viewer-next: Use npm for the frontend

--- nix/ping-viewer-next/frontend.nix
+++ nix/ping-viewer-next/frontend.nix
@@ -1,10 +1,11 @@
 {
   lib,
-  stdenvNoCC,
-  bun,
-  nodejs,
+  buildNpmPackage,
+  importNpmLock,
 }:
-let
+buildNpmPackage {
+  name = "ping-viewer-next-frontend";
+
   src = lib.fileset.toSource {
     root = ../../ping-viewer-next/ping-viewer-next-frontend;
     fileset = lib.fileset.intersection ../../ping-viewer-next/ping-viewer-next-frontend (
@@ -12,57 +13,11 @@ let
     );
   };
 
-  node_modules = stdenvNoCC.mkDerivation {
-    name = "ping-viewer-next-frontend-node-modules";
-
-    inherit src;
-
-    nativeBuildInputs = [ bun ];
-
-    dontConfigure = true;
-    dontFixup = true;
-
-    buildPhase = ''
-      runHook preBuild
-
-      bun install --frozen-lockfile --no-progress
-
-      runHook postBuild
-    '';
-
-    installPhase = ''
-      rm -rf node_modules/.cache
-      cp -R node_modules $out
-    '';
-
-    outputHashMode = "recursive";
-    outputHashAlgo = "sha256";
-    outputHash =
-      {
-        "x86_64-linux" = "sha256-Fmx1b+Xw0ij9HpK724tQD+Jc7EfDgaMRER/OAySsb84=";
-        "aarch64-linux" = "sha256-v4ufUxoImkPmYjoHs7b8ILXn1QfAZ2aq1ZlkYkowjuY=";
-      }
-      ."${stdenvNoCC.hostPlatform.system}";
+  npmDeps = importNpmLock {
+    npmRoot = ../../ping-viewer-next/ping-viewer-next-frontend;
   };
-in
-stdenvNoCC.mkDerivation {
-  name = "ping-viewer-next-frontend";
 
-  inherit src;
-
-  nativeBuildInputs = [ nodejs ];
-
-  dontConfigure = true;
-
-  buildPhase = ''
-    runHook preBuild
-
-    cp -R ${node_modules} node_modules
-    chmod -R u+w node_modules
-    node node_modules/.bin/vite build
-
-    runHook postBuild
-  '';
+  inherit (importNpmLock) npmConfigHook;
 
   installPhase = ''
     mkdir -p $out/share
